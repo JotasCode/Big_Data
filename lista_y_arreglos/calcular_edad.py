946e43d528cb6935e858d970f14e073f001c6f16
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-11-07 11:34:16 -0400

    Add files via upload

---
+++ lista_y_arreglos/calcular_edad.py
@@ -0,0 +1,15 @@
+lista_frutas = ["manzana", "pera", "melon", "piña", "sandía"]
+
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+#1- for i in range(len(lista_frutas)):
+#2- for 1 in range(5):
+#3- for 2 in range(5):
+#4- for 3 in range(5):
+#5- for 4 in range(5):
+#6- for 5 in range(5):
+
+for in
+for of
+for each 
