776287bbdc4d3dbcc3ea34c9252a3ecdada69f59
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-11-07 14:10:30 -0700

    Add files via upload

---
+++ registro_personas/main.py
@@ -0,0 +1,67 @@
+import csv
+import os
+from datetime import date, datetime
+#from tabulate import tabulate
+
+ARCHIVO_CSV = "registro_personas.csv"
+CAMPOS = [
+    "id", "cedula", "nombre", "apellido", "sexo", "fecha_nacimiento", "edad", "ocupacion", "empresa", "tipo_contrato", "es_asegurado", "tipo_sangre", "direccion", "telefono_residencial", "telefono_celular"
+]
+
+#------------- Funciones Auxiliares ---------------------
+def calcular_edad():
+    pass
+
+def inicializar_csv():
+    pass
+
+def obtener_datos():
+    pass
+def obtener_siquiente_id():
+    pass
+
+def crear_registros():
+    print("Se ha ejecutado la función 'Crear Nuevo Registro'")
+
+def leer_registros():
+    print("Se ha ejecutado la función 'Mostrar Todos los Registros'")
+
+def actualizar_registros():
+    print("Se ha ejecutado la función 'Actualizar Registro por ID'")
+
+def eleminar_registros():
+    print("Se ha ejecutado la función 'Eliminar Registro por ID'")
+
+def menu_principal():
+    #Inciando la función que crea el archivo csv, donde se guardarán los datos.
+    inicializar_csv()
+
+    #Muestra en pantalla un mensaje de bienvenida al programa.
+    print("Bienvenido/a al programa de registro de personas.")
+    while True:
+        print("1. Crear Nuevo Registro")
+        print("2. Mostrar Todos los Registros")
+        print("3. Actualizar Registro por ID")
+        print("4. Eliminar Registro por ID")
+        print("5. Salir")
+
+        opcion = input("Favor de digitar una de las opciones: ")
+
+        if (opcion == "1"):
+            crear_registros()
+        elif (opcion == "2"):
+            leer_registros()
+        elif (opcion == "3"):
+            actualizar_registros()
+        elif (opcion == "4"):
+            eleminar_registros()
+        elif (opcion == "5"):
+            print("Muchas gracias por utilizar nuestros servicios.")
+            print("JotasCode @2025 (all rights deserve)")
+        else:
+            print("Opcición no valida, favor de registrar una de las opciones indicadas.")
+        break
+
+
+if __name__ == "__main__":
+    menu_principal()
