d9afebc3ab7cc8831e6a57f6e8f191d7d880c22c
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-11-11 17:07:29 -0400

    Add files via upload

--- registro_personas/main.py
+++ registro_personas/main.py
@@ -8,20 +8,61 @@ CAMPOS = [
     "id", "cedula", "nombre", "apellido", "sexo", "fecha_nacimiento", "edad", "ocupacion", "empresa", "tipo_contrato", "es_asegurado", "tipo_sangre", "direccion", "telefono_residencial", "telefono_celular"
 ]
 
-#------------- Funciones Auxiliares ---------------------
-def calcular_edad():
-    pass
+#------------------- Funciones Auxiliares ---------------------
+def calcular_edad(fecha_nacimiento_str):
+    """Calcular la edad de una persona a partir de su fecha de nacimiento (YYYY-MM-DD)"""
+    try:
+        #Convierte la cadena de objeto date
+        fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, "%Y-%m-%d").date()
+    except ValueError:
+        return None 
+        #Retorna None si el formato es incorrecto
+    
+    hoy = date.today()
+
+    #Resta los años
+    edad = hoy.year - fecha_nacimiento.year 
+
+    #Ajustar el mes y el día por si todavía no ha cumplido años
+    if(hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento_str.day):
+        edad -= 1
+    
+    return edad 
 
 def inicializar_csv():
-    pass
+    """Crear el archivo CSV con los encabezados si no existen"""
+    if not os.path.exists(ARCHIVO_CSV)
+        with open(ARCHIVO_CSV, "w", newline="", enconding="utf-8") as archivo: 
+            writer = csv.DictWriter(archivo, fieldnames=CAMPOS)
+            writer.writeheader()
+            print(f"Archivo '{ARCHIVO_CSV}' creado con éxito.")
 
 def obtener_datos():
-    pass
+    """Lee todos los datos del CSV y los retorna como una lista de diccionarios."""
+    datos = []
 
-def obtener_siquiente_id():
-    pass
+    try:
+        with open(ARCHIVO_CSV, "r", newline= "", encoding="utf-8") as archivo:
+            reader = csv.DictReader(archivo)
 
-#------------- Funciones CRUD ---------------------
+            for fila in reader:
+                datos.append(fila)
+    
+    except FileNotFoundError:
+        pass #Si no existe, retorna lista vacía. La función crear se encarga de crearlo. 
+
+    return datos
+
+def obtener_siquiente_id(datos):
+    """Calcula el próximo ID a partir de los datos existentes."""
+    if not datos:
+        return 1
+    
+    max_id = max(int(persona["id"]) for persona in datos if persona["id"].isdigit())
+
+    return max_id + 1
+
+#------------------- Funciones CRUD ---------------------
 def crear_registros():
     print("Se ha ejecutado la función 'Crear Nuevo Registro'")
 
@@ -63,7 +104,6 @@ def menu_principal():
         else:
             print("Opcición no valida, favor de registrar una de las opciones indicadas.")
         break
-
 
 if __name__ == "__main__":
     menu_principal()
