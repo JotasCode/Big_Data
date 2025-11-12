660d92e848e0e88d6eea2c9532ce2e81bc5d10be
JotasCode <223016397+JotasCode@users.noreply.github.com>
2025-11-12 17:27:17 -0400

    Add files via upload

--- registro_personas/main.py
+++ registro_personas/main.py
@@ -17,7 +17,7 @@ def calcular_edad(fecha_nacimiento_str):
     except ValueError:
         return None 
         #Retorna None si el formato es incorrecto
-    
+
     hoy = date.today()
 
     #Resta los años
@@ -26,7 +26,7 @@ def calcular_edad(fecha_nacimiento_str):
     #Ajustar el mes y el día por si todavía no ha cumplido años
     if(hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento_str.day):
         edad -= 1 
-    
+
     return edad 
 
 def inicializar_csv():
@@ -47,7 +47,7 @@ def obtener_datos():
 
             for fila in reader:
                 datos.append(fila)
-    
+
     except FileNotFoundError:
         pass #Si no existe, retorna lista vacía. La función crear se encarga de crearlo. 
 
@@ -57,14 +57,57 @@ def obtener_siquiente_id(datos):
     """Calcula el próximo ID a partir de los datos existentes."""
     if not datos:
         return 1
-    
+
     max_id = max(int(persona["id"]) for persona in datos if persona["id"].isdigit())
 
     return max_id + 1
 
 #------------------- Funciones CRUD ---------------------
 def crear_registros():
-    print("Se ha ejecutado la función 'Crear Nuevo Registro'")
+    """Solicita los datos y crea un nuevo registro en el CSV."""
+    inicializar_csv()
+
+    datos = obtener_datos()
+    nuevo_id = obtener_siquiente_id(datos)
+
+    print("--------- Insercion de nuevo registro de persona ---------")
+    registro = {
+        "id" : str(nuevo_id)
+    }
+    
+    registro["cedula"] = input("Digite su cédula: ")
+    registro["nombre"] = input("Digite su nombre: ")
+    registro["apellido"] = input("Digite su apellido: ")
+    registro["sexo"] = input("Digite su sexo: ")
+    
+    while True:
+        fecha_nac_str = input("Fecha de Nacimiento (YYYY-MM-DD): ")
+        edad_calculada = calcular_edad(fecha_nac_str)
+        if edad_calculada is not None:
+            registro["fecha_nacimiento"] = fecha_nac_str
+            registro["edad"] = str(edad_calculada)
+            print(f"Edad calculada: {edad_calculada} años.")
+            break
+        else:
+            print("Formato de fecha incorrecto. Use el formato correcto (YYYY-MM-DD).")
+    
+    registro["ocupacion"] = input("Digite su ocupación: ")
+    registro["empresa"] = input("Digite su empresa: ")
+    registro["tipo_contrato"] = input("Digite su tipo de contrato: ")
+    registro["es_asegurado"] = input("Digite si es asegurado/a o no (sí/no): ")
+    registro["tipo_sangre"] = input("Digite su tipo de sangre: ")
+    registro["direccion"] = input("Digite su dirección: ")
+    registro["telefono_residencial"] = input("Digite su teléfono residencial: ")
+    registro["telefono_celular"] = input("Digite su teléfono celular: ")
+
+    datos.append(registro)
+
+    with open(ARCHIVO_CSV, "w", newline="", encoding="utf-8") as archivo:
+        writer = csv.DictWriter(archivo, fieldnames=CAMPOS)
+        writer.writeheader()
+        writer.writerows(datos)
+
+        print(f"Registro con el id {nuevo_id} creado y guardado con éxito.")
 
 def leer_registros():
     print("Se ha ejecutado la función 'Mostrar Todos los Registros'")
@@ -101,9 +144,9 @@ def menu_principal():
         elif (opcion == "5"):
             print("Muchas gracias por utilizar nuestros servicios.")
             print("JotasCode © 2025 (all rights reserved).")
+            break
         else:
             print("Opcición no valida, favor de registrar una de las opciones indicadas.")
-        break
 
 if __name__ == "__main__":
     menu_principal()
